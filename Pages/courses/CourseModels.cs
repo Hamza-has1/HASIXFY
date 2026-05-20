2ca3eae077b83183df456681f629d94e0bfa6fac
Hamzahash <hamzahas530@gmail.com>
2026-05-21 02:48:23 +0500

    force add framework

--- Pages/courses/CourseModels.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyPortfolio.Pages.courses
-{
-    public class ModuleItem
-    {
-        public string Title { get; set; } = "";
-        public string Topics { get; set; } = "";
-    }
-}
